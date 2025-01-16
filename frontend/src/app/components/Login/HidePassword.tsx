52dc4f9f3bc75a2e861fd7705965bfb6026dfcab
thonyS07 <thonysilva1993@gmail.com>
2025-01-16 02:19:58 -0500

    Insertando boton mostrar contraseña en el input

--- frontend/src/app/components/Login/HidePassword.tsx
+++ frontend/src/app/components/Login/HidePassword.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const HidePassword = () => {
 	return (
 		<svg
-			width='100%'
-			height='100%'
+			width='24px'
+			height='24px'
 			viewBox='0 0 64 64'
 			version='1.1'
 			xmlns='http://www.w3.org/2000/svg'
