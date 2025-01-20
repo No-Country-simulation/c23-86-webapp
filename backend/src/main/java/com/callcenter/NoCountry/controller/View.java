e0b7af4dfb47e839533177949d6677cb2bdbe6a6
Sonoda Matias <matiasnsonoda@gmail.com>
2025-01-20 14:00:20 -0300

    Update View.java

--- backend/src/main/java/com/callcenter/NoCountry/controller/View.java
+++ backend/src/main/java/com/callcenter/NoCountry/controller/View.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.callcenter.callcenter.controller;
+package com.callcenter.NoCountry.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
