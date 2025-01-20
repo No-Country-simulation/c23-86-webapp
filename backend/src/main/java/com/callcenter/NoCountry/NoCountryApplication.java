13b4d74e5ec3cdffbaa1660fc3a8269dfbc1254c
Sonoda Matias <matiasnsonoda@gmail.com>
2025-01-20 13:58:58 -0300

    Update NoCountryApplication.java

--- backend/src/main/java/com/callcenter/NoCountry/NoCountryApplication.java
+++ backend/src/main/java/com/callcenter/NoCountry/NoCountryApplication.java
@@ -1,10 +1,10 @@
-package com.callcenter.callcenter;
+package com.callcenter.NoCountry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CallcenterApplication {
+public class NoCountryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallcenterApplication.class, args);
