69bcf5d2aaf74c0184dd68bf3a8b7ba4c8d4c2fc
Jose Lambrechts <alt165@gmail.com>
2025-02-05 21:20:19 -0300

    + CorsConfig

---
+++ backend/src/main/java/com/callcenter/NoCountry/Config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.callcenter.NoCountry.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000")); // Origen permitido
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Headers permitidos
+        config.setAllowCredentials(true); // Permitir envío de cookies y autenticación
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
