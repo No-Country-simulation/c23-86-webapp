242480b501fef50482a681634e7adeab7c67d530
Jose Lambrechts <alt165@gmail.com>
2025-02-06 18:08:02 -0300

    + CorsConfig

--- backend/src/main/java/com/callcenter/NoCountry/Config/CorsConfig.java
+++ backend/src/main/java/com/callcenter/NoCountry/Config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // Origen permitido
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://c23-86-call-master.vercel.app"
+        )); // Origen permitido
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
         config.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Headers permitidos
         config.setAllowCredentials(true); // Permitir envío de cookies y autenticación
